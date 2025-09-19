c088594f5f64c2a6a51034829379435e9f17fa8e
Amruthkumar111 <238w1a02a5@vrsec.ac.in>
2025-09-19 15:07:27 +0530

    Create prod_even.py

---
+++ prod_even.py
@@ -0,0 +1,11 @@
+#product of all even from m to n
+m=int(input("ENTER M VALUE: "))
+n=int(input("ENTER N VALUE: "))
+if m>n:
+  print(0)
+else:
+  x=1
+  for i in range(m,n+1):
+    if i%2==0:
+      x=x*i
+  print("PRODUCT: ",x)
